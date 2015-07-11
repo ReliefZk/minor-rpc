950a0baa97f3cc4e6c86d88ae5c37544685f0da4
ReliefZk <reliefzk@gmail.com>
2015-07-11 11:02:10 +0800

    ConcurrentHashmap

--- src/main/java/com/zk/rpc/common/server/AbstractBeanRegister.java
+++ src/main/java/com/zk/rpc/common/server/AbstractBeanRegister.java
@@ -3,6 +3,7 @@ package com.zk.rpc.common.server;
 import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Bean注册器抽象工厂
@@ -11,8 +12,8 @@ import java.util.Map;
  */
 public abstract class AbstractBeanRegister implements BeanRegister {
 
-	protected Map<String,Object> beanMap = new HashMap<String, Object>();
-	protected Map<String,Method> methodMap = new HashMap<String, Method>();
+	protected Map<String,Object> beanMap = new ConcurrentHashMap<String, Object>();
+	protected Map<String,Method> methodMap = new ConcurrentHashMap<String, Method>();
 	protected MethodKey methodKey = null;
 	
 	public AbstractBeanRegister() {
